5755ec98939f5fb0588a8c982ea1b65357133ae7
briggySmalls <sam.briggs@entia.co>
2019-09-26 17:38:56 +0100

    Move removal of common values to notebook
    This seems outside of the scope of the function

--- src/suggest.py
+++ src/suggest.py
@@ -1,28 +1,32 @@
 """
 String comaprison logic
 """
-from Levenshtein import ratio  # pylint: disable=no-name-in-module
 import pandas as pd
+from Levenshtein import ratio  # pylint: disable=no-name-in-module
 
 
 def find_suggestions(to_fix: set, to_search: set) -> pd.DataFrame:
     """Suggests corrections for a set of strings, from provided options
 
     Args:
-        to_fix (pd.Series): List of strings to find a suggestion for
-        to_search (pd.Series): LIst of strings suggestions are selected from
+        to_fix (set): List of strings to find a suggestion for
+        to_search (set): LIst of strings suggestions are selected from
+
+    Returns:
+        pd.DataFrame: Matrix that supplies suggestions and probabilities for
+                      each string in to_fix
     """
-    common_names = to_fix.intersection(to_search)
-    to_fix_s = pd.Series(list(to_fix - common_names))
-    to_search_s = pd.Series(list(to_search - common_names))
+    # Convert the sets to pandas series (so we can build a matrix)
+    to_fix_series = pd.Series(list(to_fix))
+    to_search_series = pd.Series(list(to_search))
+
     # Create a matrix, where:
     # - each row corresponds to a string to fix
     # - each column corresponds to a suggestion
     # - each cell is the probability of the suggestion being correct
-    mat = to_fix_s.apply(
-        lambda name_to_fix: to_search_s.apply(lambda name_to_search: ratio(
-            name_to_fix, name_to_search)))
-    mat.rename(columns=to_search_s, index=to_fix_s, inplace=True)
+    mat = to_fix_series.apply(lambda name_to_fix: to_search_series.apply(
+        lambda name_to_search: ratio(name_to_fix, name_to_search)))
+    mat.rename(columns=to_search_series, index=to_fix_series, inplace=True)
     mat.index.name = "original"
 
     # Create a dataframe which holds:
